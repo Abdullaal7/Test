020a8fee05d63430221393bd2e077c9feb78ae5f
aabdelaal <X@s7rRFh-f5s>
2024-07-31 16:33:26 +0300

    add docker

--- src/main/java/eg/gov/moj/election/entity/Player.java
+++ src/main/java/eg/gov/moj/election/entity/Player.java
@@ -7,8 +7,6 @@ import javax.persistence.*;
 @Data
 @Entity
 public class Player {
-    
-    // Players Attripute 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,7 +17,7 @@ public class Player {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "device_info")
+    @Column(name = "nick_name")
     private String nickname;
   
 
